da8c632d37a6abdfdfee85f583a75dc980b24d49
kevin <gallagherkevin@ymail.com>
2016-04-08 11:05:47 +0100

    continued development on schedular

---
+++ src/core/Tester1.java
@@ -0,0 +1,11 @@
+package core;
+
+/**
+ * Created by Kevin on 4/8/2016.
+ */
+public class Tester1 {
+    public static void main(String []args){
+        Scheduler sched=new Scheduler();
+        sched.shedule();
+    }
+}
