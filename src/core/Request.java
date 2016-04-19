9c36bc3a684c55d90bd8865d315a3bf3602edf58
Ben <benjamin_sweeney@hotmail.com>
2016-04-19 10:45:57 +0100

    Added view request page

--- src/core/Request.java
+++ src/core/Request.java
@@ -1,5 +1,8 @@
 package core;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /**
  * Created by home on 11/04/2016.
  */
@@ -7,8 +10,8 @@ public class Request {
 
 
     private int request_ID;
-    private String startDate;
-    private String endDate;
+    private Calendar startDate;
+    private Calendar endDate;
     private String status;
     private int emp_ID2;
 
@@ -16,28 +19,48 @@ public class Request {
 
     }
 
-    public Request(String startDate, String endDate, int emp_ID){
+
+    public Request(int request_ID, Calendar startDate, Calendar endDate, int emp_ID){
         this.endDate=endDate;
         this.startDate=startDate;
-
+        this.request_ID=request_ID;
+        status="waiting";
+        this.emp_ID2=emp_ID;
 
 
     }
 
-    public String getStartDate() {
+    public int getEmp_ID2() {
+        return emp_ID2;
+    }
+
+    public void setEmp_ID2(int emp_ID2) {
+        this.emp_ID2 = emp_ID2;
+    }
+
+    public int getRequest_ID() {
+
+        return request_ID;
+    }
+
+    public void setRequest_ID(int request_ID) {
+        this.request_ID = request_ID;
+    }
+
+    public Calendar getStartDate() {
         return startDate;
     }
 
-    public void setStartDate(String startDate) {
+    public void setStartDate(Calendar startDate) {
         this.startDate = startDate;
     }
 
 
-    public String getEndDate() {
+    public Calendar getEndDate() {
         return endDate;
     }
 
-    public void setEndDate(String endDate) {
+    public void setEndDate(Calendar endDate) {
         this.endDate = endDate;
     }
 
@@ -47,5 +70,12 @@ public class Request {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+
+    @Override
+    public String toString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        return request_ID + "   " + startDate + "   "+ endDate+"   "+emp_ID2;
     }
 }
